c586633443493fb9abb0ac71404f0e8c7c11d7ef
JMI <ikolajm@gmail.com>
2019-03-10 13:14:37 -0400

    more simple solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'PokeApi';
-  _search = '';
   results = <any>[];
   base = 'https://pokeapi.co/api/v2/pokemon'
 
   constructor(private http: HttpClient) {}
 
-  onType(value) {
-    this._search = value;
-  }
-
-  submit() {
+  submit(search) {
     this.results = [];
-    this.http.get(`${this.base}/${this._search}`).subscribe(pokemon => {
+    this.http.get(`${this.base}/${search}`).subscribe(pokemon => {
       console.log(pokemon);
       this.results = pokemon;
     })
